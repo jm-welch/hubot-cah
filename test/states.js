da700ed141fc9d419de5a944adc0aee45e2646c4
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2015-07-29 22:44:35 -0400

    Added a beginning test states file for different db states

---
+++ test/states.js
@@ -0,0 +1,36 @@
+module.exports.joined = {
+  gameroom: null,
+  scores: {
+    bob: 0,
+    nancy: 0,
+    max: 0
+  },
+  activePlayers: ['bob', 'nancy', 'max'],
+  blackCard: '(Pick 2) Step 1: _____. Step 2: _____. Step 3: Profit.',
+  czar: 'bob',
+  hands: {
+    bob: [
+      'Pistol-whipping a hostage.',
+      'Getting so angry that you pop a boner.',
+      'Survivor\'s guilt.',
+      'Edible underpants.',
+      'Friction.'
+    ],
+    nancy: [
+      'The South.',
+      'The Three-Fifths Compromise.',
+      'Suicidal thoughts.',
+      'A cat video so cute that your eyes roll back and your spine slides out of your anus.',
+      'Crying into the pages of Sylvia Plath.'
+    ],
+    max: [
+      'The inevitable heat death of the universe.',
+      'Public ridicule.',
+      'Ethnic cleansing.',
+      'Natalie Portman.',
+      'The invisible hand.'
+    ]
+  },
+  answers: [],
+  room: undefined
+}
