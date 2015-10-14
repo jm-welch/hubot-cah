9a147192d690287d5426a16dd08e9504534ffa58
Cole Furfaro-Strode <cfurfarostrode@Coles-MacBook-Pro.local>
2015-10-13 21:25:55 -0400

    the motherload

---
+++ src/decks/expansion_3_black.js
@@ -0,0 +1,28 @@
+// sourced from https://docs.google.com/spreadsheets/d/1lsy7lIwBe-DWOi2PALZPf5DgXHx9MEvKfRw1GaWQkzg/edit#gid=9
+module.exports = [
+  "(Pick 2) _____: Hours of fun. Easy to use. Perfect for _____!",
+  "_____. Awesome in theory, kind of a mess in practice.",
+  "A successful job interview begins with a firm handshake and ends with _____.",
+  "(Pick 2) After months of practice with _____, I think I’m finally ready for _____.",
+  "And what did you bring for show and tell?",
+  "As part of his contract, Prince won’t perform without _____ in his dressing room.",
+  "As part of his daily regimen, Anderson Cooper sets aside 15 minutes for _____.",
+  "Call the law offices of Goldstein & Goldstein, because no one should have to tolerate _____ in the workplace.",
+  "During high school I never really fit in until I found _____ club.",
+  "Finally! A service that delivers _____ right to your door.",
+  "(Pick 2) Having problems with _____? Try _____!",
+  "Hey baby, come back to my place and I’ll show you _____.",
+  "I’m not like the rest of you. I’m too rich and busy for _____.",
+  "In the seventh circle of Hell, sinners must endure _____ for all eternity.",
+  "(Pick 2) Listen, son. If you want to get involved with _____, I won’t stop you. Just steer clear of _____.",
+  "Lovin’ you is easy ’cause you’re _____.",
+  "Money can’t buy me love, but it can buy me _____.",
+  "My gym teacher got fired for adding _____ to the obstacle course.",
+  "(Pick 2) My life is ruled by a vicious cycle of _____ and _____.",
+  "The blind date was going horribly until we discovered our shared interest in _____.",
+  "To prepare for his upcoming role, Daniel Day-Lewis immersed himself in the world of _____.",
+  "Turns out that _____-Man was neither the hero we needed nor wanted.",
+  "What left this stain on my couch?",
+  "(Pick 2) When you get right down to it, _____ is just _____.",
+  "(Pick 2) With enough time and pressure, _____ will turn into _____."
+];
